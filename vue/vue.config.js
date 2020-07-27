845c38b93865013895aebeb81409bcd472e8f557
srxboys <srxboys@126.com>
2020-07-27 10:17:25 +0800

    vue build file.name & update docs

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,6 +1,14 @@
 const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   publicPath: isProd ? "./" : "/",
   // 线上代码 不让看源码
   productionSourceMap: isProd ? false : true,
+
+  configureWebpack: {
+    output: {
+      filename: `js/[name].[chunkhash].js`,
+      chunkFilename: `js/[name].[chunkhash].js`
+    },
+  }
 }
