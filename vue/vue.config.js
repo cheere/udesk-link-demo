cea0484ea2408d5bbc52e48a90638122846f48f2
srxboys <srxboys@126.com>
2020-10-22 17:05:21 +0800

    demo update docs

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,7 +1,10 @@
 const isProd = process.env.NODE_ENV === "production";
+const isPage = process.env.VUE_APP_ENV === "page";
+console.log('isProd=>', isProd)
+console.log('isPage=>', isPage)
 
 module.exports = {
-  publicPath: isProd ? "./" : "/",
+  publicPath: isPage ? "./udesk-link-demo/" : "./",
   // 线上代码 不让看源码
   productionSourceMap: isProd ? false : true,
 
