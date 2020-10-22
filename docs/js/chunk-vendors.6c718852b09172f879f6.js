dd795e5912078de06f37270ac70ed2027834fb35
srxboys <srxboys@126.com>
2020-10-22 18:30:56 +0800

    demo update docs 7

--- docs/js/chunk-vendors.6c718852b09172f879f6.js
+++ docs/js/chunk-vendors.6c718852b09172f879f6.js
@@ -1,7 +1,7 @@
 /*!
  * 
  *  Version 0.1.0
- *  Pack by srxboys on 2020-10-22 18:10:35 .
+ *  Pack by srxboys on 2020-10-22 18:30:23 .
  */
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-vendors"],{"0261":function(t,e,n){"use strict";(function(t){
 /*!
